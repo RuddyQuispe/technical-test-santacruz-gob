ec3795fb4190f2035326c4de54058147622df5a3
Ruddy Quispe <ruddyq18@gmail.com>
2022-10-11 10:28:12 -0400

    poblacion de la BD y el procedimiento almacenado del ejercicio #2

--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+-- formato de fecha de la BD YYYY/MM/DD
 -- CREATE ROLE
 CREATE ROLE test_user
     WITH
@@ -11,21 +12,22 @@ CREATE ROLE test_user
 CREATE DATABASE test_gobernation
     OWNER test_user;
 
-
 create table sectores(
 	sector_id serial primary key,
 	sector_desc text not null
 );
 
 insert into sectores(sector_desc) values ('administracion'),
-										 ('desarrollo');
+										 ('desarrollo'),
+										 ('rrhh');
 
 create table tipos_accidentes(
 	ta_id serial primary key,
 	ta_desc text not null
 );
-
-insert into tipos_accidentes(ta_desc) values ('lucha libre'),('choque autos');
+insert into tipos_accidentes(ta_desc) values ('accidente vehicular'),
+											 ('sansion'),
+											 ('conflictos severos');
 
 create table empleados(
 	emp_id serial primary key,
@@ -40,11 +42,14 @@ create table empleados(
 	on update cascade
 	on delete cascade,
 	foreign key (emp_id_supervisor) references empleados(emp_id)
+	on update no action
+	on delete no action
 );
 
 insert into empleados(emp_apellido, emp_nombre, emp_sexo, emp_fecnac, emp_fecing, emp_id_supervisor, emp_sector_id) values
-('chumacero','joaquin', true, '09/09/1980', '09/10/2019', null, 1),
-('quispe','ruddy', false, '11/10/1999', '11/10/2022', 1,1);
+('chumacero','joaquin', true, '1990/01/03', '2019/05/16', null, 1),
+('quispe','ruddy', false, '1980/03/15', '2021/06/30', 1,1),
+('chavarria','marisol', true, '2000/10/20', '2022/05/3', null, 1);
 
 create table accidentes(
 	acc_id serial primary key,
@@ -63,8 +68,25 @@ create table accidentes(
 	on update cascade
 	on delete cascade
 );
-select * from accidentes a ;
 
 insert into accidentes (acc_fecha, acc_severidad, acc_emp_id, acc_sector_id, acc_ta_id) values
-('01/10/2022', 'simple', 1, 1, 1),
-('10/10/2022', 'grave', 2, 1, 1);
+('2022/09/03', 'simple', 1, 1, 1),
+('2022/09/10', 'simple', 1, 1, 1),
+('2022/09/15', 'grave', 2, 2, 2),
+('2022/09/18', 'simple', 3, 2, 1),
+('2022/09/20', 'grave', 1, 2, 2),
+('2022/09/30', 'simple', 3, 1, 1);
+
+-- funcion qu retorna una sql query
+create or replace function lista_igualxacci(first_param date, second_param date) 
+returns table(tipo_acc_id int, acc_desc text, cant_acc bigint)
+as $$ begin
+	return query select ta.ta_id, ta.ta_desc, count(distinct a.acc_id) 
+			from tipos_accidentes ta, accidentes a, sectores s
+			where ta.ta_id=a.acc_ta_id and a.acc_emp_id=s.sector_id and s.sector_id=a.acc_sector_id  
+				and first_param <= a.acc_fecha and a.acc_fecha  <= second_param 
+			group by ta.ta_id, ta.ta_desc;
+end $$ language plpgsql;
+
+-- ejecucion de la funcion con formato de fecha YYYY/MM/DD
+select * from lista_igualxacci('2022/09/9', '2022/09/30');
